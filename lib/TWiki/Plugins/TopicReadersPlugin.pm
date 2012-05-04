f0f389f8bbfe0274a91c18ba49e54858e6405601
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/TopicReadersPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/TopicReadersPlugin.pm
+++ lib/TWiki/Plugins/TopicReadersPlugin.pm
@@ -11,7 +11,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -19,15 +19,15 @@
 # This is TopicReaders TWiki plugin.
 #
 
-
 # =========================
-package TWiki::Plugins::TopicReadersPlugin;    # change the package name and $pluginName!!!
+package TWiki::Plugins::TopicReadersPlugin
+  ;    # change the package name and $pluginName!!!
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug  $DefaultReadersFormat $ToolTipID $ToolTipOpened
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug  $DefaultReadersFormat $ToolTipID $ToolTipOpened
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -39,38 +39,43 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-$pluginName = 'TopicReadersPlugin';  # Name of this Plugin
+$pluginName = 'TopicReadersPlugin';    # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
+
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
+    $debug = TWiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
 
-    $DefaultReadersFormat = &TWiki::Func::getPreferencesValue ("TOPICREADERSPLUGIN_READERSFORMAT") || "<li> %READERNAME% : %READERDATE%";
+    $DefaultReadersFormat =
+      &TWiki::Func::getPreferencesValue("TOPICREADERSPLUGIN_READERSFORMAT")
+      || "<li> %READERNAME% : %READERDATE%";
 
     # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
 
-    $ToolTipID=0;
-    $ToolTipOpened=0;
+    $ToolTipID     = 0;
+    $ToolTipOpened = 0;
 
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
     # This is the place to define customized tags and variables
     # Called by sub handleCommonTags, after %INCLUDE:"..."%
@@ -81,149 +86,154 @@ sub commonTagsHandler
 }
 
 # =========================
-sub handleReaders
-{
-  my $attr = shift;
-  use Time::gmtime;
-  use Time::Local;
+sub handleReaders {
+    my $attr = shift;
+    use Time::gmtime;
+    use Time::Local;
 
-  my ($cgiWeb,$cgiTopic,$cgiDate,$cgiFormat,$cgiTitle,$cgiHeader);
-  my $cgi = &TWiki::Func::getCgiQuery();
-  if( $cgi ) 
-  {
-    $cgiWeb   = $cgi->param('readersweb'); 
-    $cgiTopic = $cgi->param('readerstopic'); 
-    $cgiDate  = $cgi->param('readersdate');
-    $cgiFormat= $cgi->param('readersformat');
-    $cgiTitle = $cgi->param('readerstitle');
-    $cgiHeader= $cgi->param('readersheader');
-  }
-
-  my $theWeb    = &TWiki::Func::extractNameValuePair( "$attr", "WEB" )    || $cgiWeb    || "$web"; 
-  my $theTopic  = &TWiki::Func::extractNameValuePair( "$attr", "TOPIC" )  || $cgiTopic  || "$topic"; 
-  my $theDate   = &TWiki::Func::extractNameValuePair( "$attr", "DATE" )   || $cgiDate   || "1";
-  my $theFormat = &TWiki::Func::extractNameValuePair( "$attr", "FORMAT" ) || $cgiFormat || $DefaultReadersFormat;
-  my $theTitle  = &TWiki::Func::extractNameValuePair( "$attr", "TITLE" )  || $cgiTitle  || "";
-  my $theHeader = &TWiki::Func::extractNameValuePair( "$attr", "HEADER" ) || $cgiHeader || "";
-
-
-  my ($logfileLimit, $timeLimit) = GetLogTimeInfos("$theDate");
-  my $tmp=&TWiki::Func::formatTime($timeLimit, "HTTP", "gmtime");
-  $theTitle =~ s/%READERSSINCE%/$tmp/g;
-
-
-  my %readers=();
-  opendir( DIR, "$TWiki::cfg{LogDir}" );
-
-  foreach my $file ( sort readdir DIR )
-  {
-    if ( $file =~ /^log(\d\d\d\d)(\d\d)\.txt$/ ) 
-    {
-       my $year=$1;
-       my $month=$2-1;
-       my $filedate="$1$2";
-       if ( $filedate < $logfileLimit ) { next; }
-
-       my $filename="$TWiki::cfg{LogDir}/$file";
-       if ( ! -f $filename) { next; }
-       open (FILE, "<$filename");
-
-       while (<FILE>)
-       {
-         my ( undef, $date, $author, $action, $webtopic, $newname, $ip) = split('\|');
-         $webtopic =~ s/ //g;
-
-         if (    ("$webtopic" =~ /$theWeb\.$theTopic/) 
-              && (  "$action" =~ /view/i) 
-              && (    "$date" =~ /(\d+)\s(\w+)\s(\d+)\s-\s(\d+):(\d+)/)
-            )
-         {
-           my $time = timegm("00",$5,$4,$1,$month,$3);
-           if ( (defined ($time)) && ($time > $timeLimit ) )
-           {          
-              my $val = 0;
-             my $count = 0;
-             if ( defined($author) ) { ($val, $count) = split (' ',$readers{"$author"}); }
-             $count++;
-             if ( $val < $time ) { $readers{"$author"}="$time $count"; }
-           }
-         }
-       }
-       close (FILE);
+    my ( $cgiWeb, $cgiTopic, $cgiDate, $cgiFormat, $cgiTitle, $cgiHeader );
+    my $cgi = &TWiki::Func::getCgiQuery();
+    if ($cgi) {
+        $cgiWeb    = $cgi->param('readersweb');
+        $cgiTopic  = $cgi->param('readerstopic');
+        $cgiDate   = $cgi->param('readersdate');
+        $cgiFormat = $cgi->param('readersformat');
+        $cgiTitle  = $cgi->param('readerstitle');
+        $cgiHeader = $cgi->param('readersheader');
     }
-  }
-  closedir( DIR );
 
-  my $out="$theTitle";
+    my $theWeb =
+         &TWiki::Func::extractNameValuePair( "$attr", "WEB" )
+      || $cgiWeb
+      || "$web";
+    my $theTopic =
+         &TWiki::Func::extractNameValuePair( "$attr", "TOPIC" )
+      || $cgiTopic
+      || "$topic";
+    my $theDate =
+         &TWiki::Func::extractNameValuePair( "$attr", "DATE" )
+      || $cgiDate
+      || "1";
+    my $theFormat =
+         &TWiki::Func::extractNameValuePair( "$attr", "FORMAT" )
+      || $cgiFormat
+      || $DefaultReadersFormat;
+    my $theTitle =
+         &TWiki::Func::extractNameValuePair( "$attr", "TITLE" )
+      || $cgiTitle
+      || "";
+    my $theHeader =
+         &TWiki::Func::extractNameValuePair( "$attr", "HEADER" )
+      || $cgiHeader
+      || "";
 
-  if ( $theHeader ) { $out.="\n$theHeader\n"; }
+    my ( $logfileLimit, $timeLimit ) = GetLogTimeInfos("$theDate");
+    my $tmp = &TWiki::Func::formatTime( $timeLimit, "HTTP", "gmtime" );
+    $theTitle =~ s/%READERSSINCE%/$tmp/g;
 
-  foreach my $author (sort keys %readers) 
-  {
-    my ($time, $count) = split (' ',$readers{"$author"});
-    my $date=&TWiki::Func::formatTime($time, "HTTP","gmtime");
-    my $tmp="$theFormat";
-    $author =~ s/\W//go;
-    $tmp=~s/%READERNAME%/$TWiki::cfg{UsersWebName}\.$author/gi;
-    $tmp=~s/%READERDATE%/$date/gi;
-    $tmp=~s/%READERCOUNT%/$count/gi;
-    $tmp=~s/\|$/\|\n/;
-    $out="$out $tmp";
-  }
+    my %readers = ();
+    opendir( DIR, "$TWiki::cfg{LogDir}" );
 
-  return ("$out");
+    foreach my $file ( sort readdir DIR ) {
+        if ( $file =~ /^log(\d\d\d\d)(\d\d)\.txt$/ ) {
+            my $year     = $1;
+            my $month    = $2 - 1;
+            my $filedate = "$1$2";
+            if ( $filedate < $logfileLimit ) { next; }
+
+            my $filename = "$TWiki::cfg{LogDir}/$file";
+            if ( !-f $filename ) { next; }
+            open( FILE, "<$filename" );
+
+            while (<FILE>) {
+                my ( undef, $date, $author, $action, $webtopic, $newname, $ip )
+                  = split('\|');
+                $webtopic =~ s/ //g;
+
+                if (   ( "$webtopic" =~ /$theWeb\.$theTopic/ )
+                    && ( "$action" =~ /view/i )
+                    && ( "$date"   =~ /(\d+)\s(\w+)\s(\d+)\s-\s(\d+):(\d+)/ ) )
+                {
+                    my $time = timegm( "00", $5, $4, $1, $month, $3 );
+                    if ( ( defined($time) ) && ( $time > $timeLimit ) ) {
+                        my $val   = 0;
+                        my $count = 0;
+                        if ( defined($author) ) {
+                            ( $val, $count ) =
+                              split( ' ', $readers{"$author"} );
+                        }
+                        $count++;
+                        if ( $val < $time ) {
+                            $readers{"$author"} = "$time $count";
+                        }
+                    }
+                }
+            }
+            close(FILE);
+        }
+    }
+    closedir(DIR);
+
+    my $out = "$theTitle";
+
+    if ($theHeader) { $out .= "\n$theHeader\n"; }
+
+    foreach my $author ( sort keys %readers ) {
+        my ( $time, $count ) = split( ' ', $readers{"$author"} );
+        my $date = &TWiki::Func::formatTime( $time, "HTTP", "gmtime" );
+        my $tmp = "$theFormat";
+        $author =~ s/\W//go;
+        $tmp    =~ s/%READERNAME%/$TWiki::cfg{UsersWebName}\.$author/gi;
+        $tmp    =~ s/%READERDATE%/$date/gi;
+        $tmp    =~ s/%READERCOUNT%/$count/gi;
+        $tmp    =~ s/\|$/\|\n/;
+        $out = "$out $tmp";
+    }
+
+    return ("$out");
 }
 
+sub GetLogTimeInfos {
 
+    my $date = shift;
 
-sub GetLogTimeInfos
-{
+    my $after = 0;
+    my $timetag;
+    my $mon;
+    my $year;
 
-  my $date = shift;
-
-  my $after=0;
-  my $timetag;
-  my $mon; 
-  my $year;
-
-  if ($date =~ /(AFTER|BEFORE|>|<)/i)  
-  { 
-    my $key=$1;
-    $date =~ s/$key//;
-    if ( $key =~ /(BEFORE|<)/i) { $after=0; }
-    if ( $key =~ /(AFTER|>)/i)  { $after=1; }
-  }
-
-  if ( $after ) 
-  {                     # AFTER a date 
-    $date =~ s#/##g;
-    $date =~ s# ##g;
-
-    if ( $date =~ /^\d\d\d\d$/ ) { $date .= "01"; }
-    if ( $date =~ /^(\d\d\d\d)(\d\d)$/ ) 
-    { 
-      $mon  = $2; 
-      $year = $1;
-      $timetag = timelocal(1,1,1,1,$mon,($year-1900));
+    if ( $date =~ /(AFTER|BEFORE|>|<)/i ) {
+        my $key = $1;
+        $date =~ s/$key//;
+        if ( $key =~ /(BEFORE|<)/i ) { $after = 0; }
+        if ( $key =~ /(AFTER|>)/i )  { $after = 1; }
     }
-  }
-  else
-  {                         # before an amount of time in days, month or years
-    my $value=1;            # One year is default 
-    my $timebase=60*60*24;  # One year is default 
-    if ($date =~ /(\d+)/i)  { $value=abs($1); }
-    if ( $date =~ /D/i)     { $timebase=60*60*24; }
-    if ( $date =~ /M/i)     { $timebase=60*60*24*28; }
 
-    $timetag = time()-($timebase*$value);
-    my ( $a, $b, $c, $d, $amon, $ayear) = localtime( $timetag );
-    $year = sprintf("%.4u", $ayear + 1900);
-    $mon = sprintf("%.2u", $amon + 1);
-  }
+    if ($after) {    # AFTER a date
+        $date =~ s#/##g;
+        $date =~ s# ##g;
 
-  return ($year.$mon, $timetag);
+        if ( $date =~ /^\d\d\d\d$/ ) { $date .= "01"; }
+        if ( $date =~ /^(\d\d\d\d)(\d\d)$/ ) {
+            $mon     = $2;
+            $year    = $1;
+            $timetag = timelocal( 1, 1, 1, 1, $mon, ( $year - 1900 ) );
+        }
+    }
+    else {           # before an amount of time in days, month or years
+        my $value    = 1;               # One year is default
+        my $timebase = 60 * 60 * 24;    # One year is default
+        if ( $date =~ /(\d+)/i ) { $value    = abs($1); }
+        if ( $date =~ /D/i )     { $timebase = 60 * 60 * 24; }
+        if ( $date =~ /M/i )     { $timebase = 60 * 60 * 24 * 28; }
+
+        $timetag = time() - ( $timebase * $value );
+        my ( $a, $b, $c, $d, $amon, $ayear ) = localtime($timetag);
+        $year = sprintf( "%.4u", $ayear + 1900 );
+        $mon  = sprintf( "%.2u", $amon + 1 );
+    }
+
+    return ( $year . $mon, $timetag );
 }
-
-
 
 1;
